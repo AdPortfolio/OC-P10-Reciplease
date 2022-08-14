f0f473528d7517f3ecb4f1deb589473bbe17340b
AdPiscinam <lab@walimaloui.com>
2022-08-14 03:53:23 +0200

    Create Navigation Controller base class

--- OC-P10-Reciplease/View Controllers/NavigationController.swift
+++ OC-P10-Reciplease/View Controllers/NavigationController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class NavigationController: UINavigationController, UINavigationControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
