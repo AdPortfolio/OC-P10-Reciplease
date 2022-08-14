fdf79f7c029c1aed1a7928af1ddf8c67fdf75787
AdPiscinam <lab@walimaloui.com>
2022-08-14 04:03:39 +0200

    Create Details Coordinator class

---
+++ OC-P10-Reciplease/Coordinator/Child Coordinators/DetailsCoordinator.swift
@@ -0,0 +1,19 @@
+//
+//  DetailsCoordinator.swift
+//  OC-P10-Reciplease
+//
+//  Created by Walim Aloui on 14/08/2022.
+//
+
+import UIKit
+
+final class DetailsCoordinator: Coordinator {
+    
+    // MARK: - Properties
+    private let navigationController:  NavigationController
+    
+    init(navigationController: NavigationController) {
+        self.navigationController = navigationController
+        super.init()
+    }
+}
