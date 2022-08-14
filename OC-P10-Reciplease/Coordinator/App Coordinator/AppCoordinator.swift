d421ac006a83caa63a86b539dbf050bd6711a2ef
AdPiscinam <lab@walimaloui.com>
2022-08-14 03:54:54 +0200

    Create App Coordinator class

---
+++ OC-P10-Reciplease/Coordinator/App Coordinator/AppCoordinator.swift
@@ -0,0 +1,40 @@
+//
+//  AppCoordinator.swift
+//  OC-P10-Reciplease
+//
+//  Created by Walim Aloui on 14/08/2022.
+//
+
+import UIKit
+
+final class AppCoordinator: Coordinator {
+    
+    // MARK: - Properties
+    private let tabBarController = UITabBarController()
+    var rootViewController: UIViewController {
+        return tabBarController
+    }
+    
+    let searchCoordinator = SearchCoordinator()
+    let favoritesCoodinator = FavoritesCoordinator()
+    
+    override init() {
+        super.init()
+        NotificationCenter.default.addObserver(self, selector: #selector(self.showFavoritesScreen), name: Notification.Name("FavoritesQA"), object: nil)
+        
+        tabBarController.viewControllers = [searchCoordinator.rootViewController, favoritesCoodinator.rootViewController]
+        
+        childCoordinators.append(searchCoordinator)
+        childCoordinators.append(favoritesCoodinator)
+    }
+    
+    // MARK: - Methods
+    override func start() {
+        searchCoordinator.start()
+        favoritesCoodinator.start()
+    }
+    
+    @objc private func showFavoritesScreen() {
+        tabBarController.selectedIndex = 1
+    }
+}
