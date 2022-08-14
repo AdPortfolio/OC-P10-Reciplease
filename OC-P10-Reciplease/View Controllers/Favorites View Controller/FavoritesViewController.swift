53a88396e307ea656e745c17a8abce7882eba1c0
AdPiscinam <lab@walimaloui.com>
2022-08-14 04:15:44 +0200

    Create Search and Favorites View Controllers

---
+++ OC-P10-Reciplease/View Controllers/Favorites View Controller/FavoritesViewController.swift
@@ -0,0 +1,16 @@
+//
+//  FavoritesViewController.swift
+//  OC-P10-Reciplease
+//
+//  Created by Walim Aloui on 14/08/2022.
+//
+
+import UIKit
+
+class FavoritesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .orange
+    }
+}
---
+++ OC-P10-Reciplease/View Controllers/Search View Controller/SearchViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SearchViewController.swift
+//  OC-P10-Reciplease
+//
+//  Created by Walim Aloui on 14/08/2022.
+//
+
+import UIKit
+
+final class SearchViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+    }
+}
