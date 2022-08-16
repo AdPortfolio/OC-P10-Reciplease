dd88863aea76f0a458f81d3f13bf0b2b28945e12
AdPiscinam <lab@walimaloui.com>
2022-08-16 14:14:12 +0200

    Add Keyboard hiding when the screen is tapped and the keyboard is displayed as a UIViewController extension

---
+++ OC-P10-Reciplease/Extensions/UIViewController+EndEditing.swift
@@ -0,0 +1,20 @@
+//
+//  UIViewController+EndEditing.swift
+//  OC-P10-Reciplease
+//
+//  Created by Walim Aloui on 15/08/2022.
+//
+
+import UIKit
+
+extension UIViewController {
+    func hideKeyboardWhenTappedAround() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
