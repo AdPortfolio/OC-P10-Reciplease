17775192bd6a2fc4469c26bd36bf4d619b96305d
AdPiscinam <lab@walimaloui.com>
2022-08-20 21:29:47 +0200

    Change CoreDataStack swift file to Persistence file and create Persistence Controller class

---
+++ OC-P10-Reciplease/CoreData/Persistence.swift
@@ -0,0 +1,45 @@
+//
+//  CoreDataStack.swift
+//  OC-P10-Reciplease
+//
+//  Created by Walim Aloui on 14/08/2022.
+//
+import CoreData
+
+struct PersistenceController {
+    static let shared = PersistenceController()
+    
+    static var preview: PersistenceController = {
+        let result = PersistenceController(inMemory: true)
+        let viewContext = result.container.viewContext
+        // dummy data will go here later
+        do {
+            try viewContext.save()
+        } catch {
+            let nsError = error as NSError
+            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+        }
+        return result
+    }()
+    
+    let container: NSPersistentContainer
+    
+    init(inMemory: Bool = false) {
+        container = NSPersistentContainer(name: "Recipes")
+        
+        if inMemory {
+            container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
+        }
+        
+        container.loadPersistentStores { _, error in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+        
+        container.viewContext.automaticallyMergesChangesFromParent = true
+        container.viewContext.name = "viewContext"
+        container.viewContext.mergePolicy = NSMergeByPropertyObjectTrumpMergePolicy
+        container.viewContext.shouldDeleteInaccessibleFaults = true
+    }
+}
