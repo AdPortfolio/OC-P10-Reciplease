e7269c7907cb12e39ed84aad64d020af82a9fb60
AdPiscinam <lab@walimaloui.com>
2022-08-24 00:08:41 +0200

    Create Search VM Tests file

---
+++ OC-P10-RecipleaseTests/Search VM Test Group/SearchVMTests.swift
@@ -0,0 +1,22 @@
+//
+//  SearchVMTests.swift
+//  OC-P10-RecipleaseTests
+//
+//  Created by Walim Aloui on 23/08/2022.
+//
+
+import XCTest
+
+
+class SearchVMTests: XCTestCase {
+
+    
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+}
