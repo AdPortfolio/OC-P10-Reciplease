fa58cfeb5edea0f4f90f2a1749391922cc3e8a42
AdPiscinam <lab@walimaloui.com>
2022-08-24 11:23:17 +0200

    Add Search Results View Model tests

--- OC-P10-RecipleaseTests/Search Results VM Test Group/SearchResultsVMTests.swift
+++ OC-P10-RecipleaseTests/Search Results VM Test Group/SearchResultsVMTests.swift
@@ -12,31 +12,28 @@ import Foundation
 
 class SearchResultsVMTests: XCTestCase {
     
-//    var sut: SearchResultsViewModel!
-//    var network: RecipeNetworkType!
-//
-//    override func setUpWithError() throws {
-//        network = MockRecipesNetwork(label: "", image: "", url: "", yield: 0, ingredientsLines: [""], totalTime: 0)
-//        sut = SearchResultsViewModel(network: network, ingredients: "")
-//    }
-//
-//    override func tearDownWithError() throws {
-//        network = nil
-//        sut = nil
-//    }
-//
-//    func testGivenNoCallIsMade_WhenViewLoads_ThenLabelsAreSetCorrectly() {
-//        let expectation = self.expectation(description: "Incorrect data")
-//        let expectedResult = "Reciplease"
-//
-//        sut.titleText = { title in
-//            XCTAssertEqual(title, expectedResult)
-//            expectation.fulfill()
-//        }
-//
-//        sut.viewDidLoad()
-//        waitForExpectations(timeout: 1.0, handler: nil)
-//    }
+    var sut: SearchResultsViewModel!
+    var network: RecipeNetworkType!
 
-   
+    override func setUpWithError() throws {
+        network = MockRecipesNetwork(label: "", image: "", url: "", yield: 0, ingredientsLines: [""], totalTime: 0)
+        sut = SearchResultsViewModel(network: network, ingredients: "")
+    }
+
+    override func tearDownWithError() throws {
+        network = nil
+        sut = nil
+    }
+    
+    func testGivenNoCallIsMade_WhenViewLoads_ThenLabelsAreSetCorrectly() {
+        let expectation = self.expectation(description: "Incorrect data")
+        let expectedResult = "Reciplease"
+
+        sut.titleText = { title in
+            XCTAssertEqual(title, expectedResult)
+            expectation.fulfill()
+        }
+        sut.viewDidLoad()
+        waitForExpectations(timeout: 1.0, handler: nil)
+    }
 }
