b4e20546f05b8913ed0d9456022816e05f2ccba4
AdPiscinam <lab@walimaloui.com>
2022-08-25 19:16:30 +0200

    Add tests for checkExistence and removeRecipe methods in Details View Model Tests file

--- OC-P10-RecipleaseTests/Details VM Tests Group/DetailsVMTests.swift
+++ OC-P10-RecipleaseTests/Details VM Tests Group/DetailsVMTests.swift
@@ -4,14 +4,14 @@
 //
 //  Created by Walim Aloui on 24/08/2022.
 //
-
+import CoreData
 import XCTest
 @testable import OC_P10_Reciplease
 
 class DetailsVMTests: XCTestCase {
     
     var sut: DetailsViewModel!
-
+    
     override func setUpWithError() throws {
         let recipeVM = RecipeCellViewModel(label: "",
                                            image: "",
@@ -22,22 +22,45 @@ class DetailsVMTests: XCTestCase {
                                            favorites: false)
         sut = DetailsViewModel(recipeCellViewModel: recipeVM)
     }
-
+    
     override func tearDownWithError() throws {
         sut = nil
     }
-
+    
     func testGivenViewModel_WhenViewModelIsHit_ThenOutputIsCorrect() {
         let expectation = self.expectation(description: "Incorrect data")
         let expectedResult = "Reciplease"
-
+        
         sut.titleText = { title in
             XCTAssertEqual(title, expectedResult)
             expectation.fulfill()
         }
-
+        
         sut.viewDidLoad()
         waitForExpectations(timeout: 1.0, handler: nil)
     }
-
+    
+    func testGivenOneRecipe_WhenWeWantToCheckIfTheSameExistsInMemory_ThenReturnedAnswerIsCorrect() {
+        let managedObjectContext = setUpInMemoryManagedObjectContext()
+        _ = NSEntityDescription.insertNewObject(forEntityName: "Recipe", into: managedObjectContext)
+       
+        let recipeCellVM = RecipeCellViewModel(label: "", image: "", url: "", yield: 1.0, ingredientLines: [""], totalTime: 0.1, favorites: false)
+        sut.checkExistence()
+        
+        XCTAssertEqual(managedObjectContext.registeredObjects.count, [recipeCellVM].count)
+    }
+    
+    func testSavedRecipe_WhenAskedToDelete_ThenRecipeIsCorrectlyDeletedFromMemory() {
+        let managedObjectContext = setUpInMemoryManagedObjectContext()
+       _ = NSEntityDescription.insertNewObject(forEntityName: "Recipe", into: managedObjectContext)
+        
+        let recipeCellVM = RecipeCellViewModel(label: "", image: "", url: "", yield: 1.0, ingredientLines: [""], totalTime: 0.1, favorites: false)
+        sut.recipeCellViewModel = recipeCellVM
+        sut.addRecipe()
+        try? managedObjectContext.save()
+        print(managedObjectContext.registeredObjects)
+        sut.removeRecipe()
+       
+        XCTAssertEqual(managedObjectContext.registeredObjects.count, 0)
+    }
 }
