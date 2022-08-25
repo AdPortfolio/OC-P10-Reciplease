f669bb08d5042ff628ca1a4da0feebb91c15ae99
AdPiscinam <lab@walimaloui.com>
2022-08-25 19:17:20 +0200

    Add remaining tests for Favorites VM Tests file

--- OC-P10-RecipleaseTests/Favorites VM Tests Group/FavoritesVMTests.swift
+++ OC-P10-RecipleaseTests/Favorites VM Tests Group/FavoritesVMTests.swift
@@ -27,9 +27,26 @@ class FavoritesVMTests: XCTestCase {
         sut = nil
     }
     
-    func testClear() {
+    func testGivenDataBase_WhenClearDataBaseIsHit_ThenDataBaseIsCorrectlyCleared() {
         sut.clearDataBase()
-      
         XCTAssertEqual(managedContext.registeredObjects, Set([]))
+    }
+    
+    func testGivenRecipeCellModel_WhenRecipeCellViewModelIsCreated_ThenSavedRecipeIsCorrect() {
+        managedContext = PersistenceController.shared.container.viewContext
+        let recipe = Recipe(context: managedContext)
+        let result = sut.createCellModel(recipe: recipe)
+        
+        let recipeCellVM = RecipeCellViewModel(label: "", image: "", url: "", yield: 0.0, ingredientLines: [], totalTime: 0.0, favorites: false)
+        
+        XCTAssertEqual(result, recipeCellVM)
+    }
+    
+    func testGivenArrayOfRecipeCellViewModels_WhenARecipeIsChosen_ThenTheResultRecipeIsCorrect() {
+        managedContext = PersistenceController.shared.container.viewContext
+        let recipeCellVM = RecipeCellViewModel(label: "label", image: "image", url: "url", yield: 1.0, ingredientLines: ["ingredientLines"], totalTime: 0.1, favorites: false)
+        sut.recipeCellViewModels = [recipeCellVM]
+        let result = sut.getCellViewModel(at: IndexPath(row: 0, section: 0))
+        XCTAssertEqual(result, recipeCellVM)
     }
 }
